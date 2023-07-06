c9c62b789c697b4344553b6e3c38721e4784acd3
Sailesh1993 <karki.saileshnep@gmail.com>
2023-07-06 20:19:24 +0300

    created employees table and modify createDatabase.sql

--- querries/createDatabase.sql
+++ querries/createDatabase.sql
@@ -13,3 +13,41 @@ TABLESPACE pg_default;
 
 ALTER TABLE IF EXISTS "Application".titles
     OWNER to postgres;
+
+-- Table: Application.employees
+
+-- DROP TABLE IF EXISTS "Application".employees;
+
+CREATE TABLE IF NOT EXISTS "Application".employees
+(
+    employee_id integer NOT NULL,
+    first_name character varying COLLATE pg_catalog."default" NOT NULL,
+    last_name character varying COLLATE pg_catalog."default" NOT NULL,
+    hire_date date NOT NULL,
+    hourly_salary numeric(7,5) NOT NULL,
+    title_id integer NOT NULL,
+    manager_id integer,
+    team integer,
+    CONSTRAINT employees_pkey PRIMARY KEY (employee_id),
+    CONSTRAINT manager_id FOREIGN KEY (manager_id)
+        REFERENCES "Application".employees (employee_id) MATCH SIMPLE
+        ON UPDATE CASCADE
+        ON DELETE CASCADE
+        NOT VALID,
+    CONSTRAINT team FOREIGN KEY (team)
+        REFERENCES "Application".teams (team_id) MATCH SIMPLE
+        ON UPDATE CASCADE
+        ON DELETE CASCADE
+        NOT VALID,
+    CONSTRAINT title_id FOREIGN KEY (title_id)
+        REFERENCES "Application".titles (title_id) MATCH SIMPLE
+        ON UPDATE CASCADE
+        ON DELETE CASCADE
+        NOT VALID
+)
+
+TABLESPACE pg_default;
+
+ALTER TABLE IF EXISTS "Application".employees
+    OWNER to postgres;
+
