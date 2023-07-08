03be4bf8fe12cc952c1dea863b48e449ce843765
Sailesh1993 <karki.saileshnep@gmail.com>
2023-07-08 15:54:42 +0300

    modify the function.sql

--- querries/functions.sql
+++ querries/functions.sql
@@ -1,27 +1,33 @@
+CREATE FUNCTION track_working_hours(
+	p_employee_id INTEGER, 
+	p_project_id INTEGER, 
+	p_total_hours NUMERIC
+) RETURNS VOID AS $$
 BEGIN
 	IF NOT EXISTS (
-	SELECT 1
+		SELECT 1
 		FROM "Application".employees
 		WHERE employee_id = p_employee_id
 	) THEN
-	RAISE EXCEPTION 'Employee with ID % doesnot exist', p_employee_id;
+		RAISE EXCEPTION 'Employee with ID % does not exist', p_employee_id;
 	END IF;
-	
-	IF NOT EXISTS(
+
+	IF NOT EXISTS (
 		SELECT 1
-		FROM "Application.project"
+		FROM "Application".project
 		WHERE project_id = p_project_id
-	) THEN 
-	  RAISE EXCEPTION 'Project with id doesnot exist',p_project_id;
-	  End IF;
+	) THEN
+		RAISE EXCEPTION 'Project with ID % does not exist', p_project_id;
+	END IF;
 	  
 	IF NOT EXISTS (
-	  	SELECT 1
-		FROM "Application".teams AS t ON tp.team_id = t.team_id
-		JOIN "Application".employess AS e ON t.team_id = e.team
+		SELECT 1
+		FROM "Application".team_project AS tp
+		JOIN "Application".teams AS t ON tp.team_id = t.team_id
+		JOIN "Application".employees AS e ON t.team_id = e.team
 		WHERE e.employee_id = p_employee_id
-		AND tp.project_id = p_project_id
-	) 	THEN
+			AND tp.project_id = p_project_id
+	) THEN
 		RAISE EXCEPTION 'Employee with ID % is not assigned to Project with ID %', p_employee_id, p_project_id;
 	END IF;
 
@@ -32,7 +38,6 @@ BEGIN
 	INSERT INTO "Application".hour_tracking (employee_id, project_id, total_hours)
 	VALUES (p_employee_id, p_project_id, p_total_hours);
 END;
-SELECT "Application".track_working_hours(20,7, 40.50)
-SELECT "Application".track_working_hours(12,7,32.25)
-SELECT "Application".track_working_hours(12,7,22.75)
+$$ LANGUAGE plpgsql;
+
 
